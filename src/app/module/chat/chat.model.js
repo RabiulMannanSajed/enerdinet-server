0c94e77f60bdbfe6c3e763d41a4fc4539242135a
RabiulMannanSajed <rabiulmannansajed@gmail.com>
2025-11-09 17:02:00 +0600

    updated

--- src/app/module/chat/chat.model.js
+++ src/app/module/chat/chat.model.js
@@ -1,22 +1,27 @@
 import { model, Schema } from "mongoose";
 
-const chatSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
+const chatSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-  isRead: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const Chat = model("Chat", chatSchema);
